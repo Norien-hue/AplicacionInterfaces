e718cac6764349c7af9598d6419c1cd31b4d5044
Norien-euh <norah2006norah@outlook.com>
2025-12-09 14:00:02 +0100

    eliminados cosas que no se usaban

--- app/src/main/java/com/javafx/reciWins/utiles/AntiStaticObservable.java
+++
@@ -1,9 +0,0 @@
-package com.javafx.reciWins.utiles;
-
-import com.javafx.reciWins.controllers.MainController;
-
-public class AntiStaticObservable { //odio que los observable list no actualicen si se hace en un contexto externo a javaFX
-    public AntiStaticObservable(){
-        
-    }
-}
